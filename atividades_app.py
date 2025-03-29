57fa80c8ca19c26359e2386c565bfdb1a849cf0e
contaprasopa <thiagonxbox360@gmail.com>
2025-03-29 00:21:16 -0300

    Update atividades_app.py

--- atividades_app.py
+++ atividades_app.py
@@ -3,26 +3,8 @@ from datetime import datetime, date
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-def app():
+def app(db):
     st.title("Coisas que a lindinha tem que fazer")
-
-    if not firebase_admin._apps:
-        cred = credentials.Certificate({
-            "type": st.secrets["firebase"]["type"],
-            "project_id": st.secrets["firebase"]["project_id"],
-            "private_key_id": st.secrets["firebase"]["private_key_id"],
-            "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-            "client_email": st.secrets["firebase"]["client_email"],
-            "client_id": st.secrets["firebase"]["client_id"],
-            "auth_uri": st.secrets["firebase"]["auth_uri"],
-            "token_uri": st.secrets["firebase"]["token_uri"],
-            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-            "universe_domain": st.secrets["firebase"]["universe_domain"]
-        })
-        firebase_admin.initialize_app(cred)
-
-    db = firestore.client()
 
     atividades_ref = db.collection("atividades").order_by("data")
     docs = atividades_ref.stream()
